47c198966cc00459386b72a02bec12f561d317ca
SamuelWango <wangos478@gmail.com>
2023-06-17 12:45:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return "This Saturday, I want to " + activity + "!";
+  }
+  
+  
+  const mondayWork = function(activity = "go to the office") {
+    return "This Monday, I will " + activity + ".";
+  }
+  
+  function wrapAdjective(flair=`*`){
+    return function encouragingPrompt(adjective=`special`){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+  }
+  
